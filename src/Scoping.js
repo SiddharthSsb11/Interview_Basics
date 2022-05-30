14865de5273dcac0af87057f11eae742895f5faf
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-05-30 23:47:01 +0530

    scoping added

---
+++ src/Scoping.js
@@ -0,0 +1,57 @@
+/* function a() {
+  console.log(b); // 10
+  // Instead of printing undefined it prints 10, So somehow this a function could access
+  //the variable b outside the function scope.
+}
+
+var b = 2;
+a();
+//var b = 10;  line 2 prints undefined in this case */
+
+// CASE 1
+/* function a() {
+  console.log(b); // 10
+  // Instead of printing undefined it prints 10, So somehow this a function could access the variable b outside the function scope.
+}
+var b = 10;
+a(); */
+
+// CASE 2
+/* function a() {
+  c();
+  function c() {
+    console.log(b); // 10
+  }
+}
+var b = 10;
+a(); */
+
+// CASE 3
+/* function a() {
+  c();
+  function c() {
+    var b = 100;
+    console.log(b); // 100
+  }
+}
+var b = 10;
+a(); */
+
+// CASE 4
+function a() {
+  var b = 10;
+  c();
+  function c() {
+    console.log(b); // 10
+  }
+}
+a();
+console.log(b); // Error, Not Defined
+
+/* 
+Global {
+    Outer {
+        Inner
+    }
+}*/
+// Inner is surrounded by lexical scope of Outer */
