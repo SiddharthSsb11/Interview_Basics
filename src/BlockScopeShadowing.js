6c8aaa93b94e2c9697b34c1d1f8bd3338a106ee5
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-05-31 23:17:47 +0530

    block scoping and shadowing added

---
+++ src/BlockScopeShadowing.js
@@ -0,0 +1,58 @@
+//BLOCK SCOPE
+/* 
+{
+  var a = 10;
+  let b = 20;
+  const c = 30;
+
+  console.log(a) // Here let and const are hoisted in Block scope, // While, var is hoisted in Global scope.
+  console.log(b)
+  console.log(c)
+}
+
+
+console.log(a)
+console.log(b)  // Uncaught ReferenceError: b is not defined
+ */
+
+//SHADOWING
+
+//Case 1
+/* var a = 100;
+{
+  var a = 10; // same name as global var
+  let b = 20;
+  const c = 30;
+  console.log(a); // 10
+  console.log(b); // 20
+  console.log(c); // 30
+}
+console.log(a); */ // 10, instead of the 100 we were expecting. So block "a" modified val of
+//global "a" as well. In console, only b and c are in block space. a initially is in global
+//space(a = 100), and when a = 10 line is run, a is not created in block space, but replaces
+//100 with 10 in global space itself.
+//If one has same named variable outside the block, the variable inside the block shadows the outside variable.
+//This happens only for var
+
+//Case 2
+
+/* let b = 100;
+{
+var a = 10;
+let b = 20;
+const c = 30;
+console.log(b); // 20
+}
+console.log(b); */ // 100, Both b's are in separate spaces (one in Block(20) and one in
+//Script(another arbitrary mem space)(100)). Same is also true for *const* declarations.
+
+//Same for functions
+/* const c = 100;
+function x() {
+  const c = 10;
+  console.log(c); // 10
+}
+x();
+console.log(c); */ // 100 //same for functions
+
+
