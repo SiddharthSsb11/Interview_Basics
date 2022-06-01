1c392e978cd77c61b5f6ee6259741e91670705e1
SiddharthSsb11 <siddharthbhadoriya1@gmail.com>
2022-06-01 23:53:01 +0530

    closures added

--- src/Closures.js
+++ src/Closures.js
@@ -7,3 +7,13 @@ function x() {
 }
 var z = x();
 console.log(z); // value of z is entire code of function y.
+
+/* 
+In above code, When y is returned, not only is the function returned but the entire closure (fun y + its lexical
+    scope) is returned and put inside z. So when z is used somewhere else in program, it still remembers var a
+    inside x()
+
+ Thus In simple words, we can say:
+    A closure is a function that has access to its outer function scope even after the function has returned.
+    Meaning, A closure can remember and access variables and arguments reference of its outer function even after
+    the function has returned. */
